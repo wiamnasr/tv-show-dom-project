2f8aa89319c77b01981b9751bd2117fddd2344e3
Wiam <wiamnasr@gmail.com>
2021-10-24 10:51:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,61 +14,55 @@ function makePageForEpisodes(episodeList) {
   episodeList.forEach((episode) => {
     let currEpisode = document.createElement("div");
     let currEpisodeHeading = document.createElement("h2");
-    currEpisodeHeading.textContent = `Episode's Name: ${episode.name} - S0${
+    currEpisodeHeading.textContent = `${episode.name} - S0${
       episode.season
     }E${episode.number < 10 ? "0" + episode.number : episode.number}`;
     let episodeDescription = document.createElement("p");
     episodeDescription.innerHTML = episode.summary;
+    episodeDescription.addEventListener(
+      "mouseover",
+      function (event) {
+        // highlight the mouseover target
+        event.target.style.color = "orange";
+        
+          // reset the color after a short delay
+          setTimeout(function () {
+            event.target.style.color = "";
+            
+          }, 100);
+      },
+      false
+    );
     let episodeImg = document.createElement("img");
     episodeImg.src = episode.image.medium;
-    
-    
+
     rootElem.appendChild(currEpisode);
     currEpisode.appendChild(currEpisodeHeading);
-    currEpisodeHeading.appendChild(episodeDescription);
+    currEpisode.appendChild(episodeDescription);
     currEpisode.appendChild(episodeImg);
 
-
-
-    currEpisode.style.width = "25%";
+    currEpisode.style.width = "20%";
     currEpisode.style.height = "500px";
     currEpisode.style.border = "none";
     currEpisode.style.margin = "50px 0 0 50px";
     currEpisode.style.backgroundColor = "rgba(0,120,80,0.9)";
     currEpisode.style.color = "white";
 
+    currEpisodeHeading.style.width = "90%";
+    currEpisodeHeading.style.margin = "5px 0 0 5%";
 
-    currEpisodeHeading.style.width = "100%";
-    currEpisodeHeading.style.margin = "0 0 0 0";
-    currEpisodeHeading.style.height = "10%";
     currEpisodeHeading.style.border = "solid";
-    currEpisodeHeading.style.borderRadius = "20%";
+    currEpisodeHeading.style.borderRadius = "5%";
 
     episodeDescription.style.width = "90%";
     episodeDescription.style.fontSize = "13px";
-    // episodeDescription.style.margin = "10% 0 0 5%";
-    episodeDescription.style.height = "50%";
+    episodeDescription.style.padding = "10% 0 0 5%";
+    episodeDescription.style.height = "170px";
 
-    
-
-    episodeImg.style.width = "100%";
-    episodeImg.style.height = "40%";
-    episodeImg.style.margin = "55% 0 0 0"
-
-     
-    
-    
-    
-
-
-
-    
-    
-
+    episodeImg.style.width = "80%";
+    episodeImg.style.height = "140px";
+    episodeImg.style.margin = "5% 0 0 10%";
   });
- 
-  
-
 }
 
 window.onload = setup;
