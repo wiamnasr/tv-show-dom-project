1853ebddb1b2303052e4ef7469f0a2534a7cd673
Wiam <wiamnasr@gmail.com>
2021-10-24 12:13:12 +0100

    Done with level 100

--- script.js
+++ script.js
@@ -6,17 +6,40 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
+  let mainHeader = document.createElement("h1");
+
   rootElem.style.display = "flex";
   rootElem.style.flexFlow = "row wrap";
   rootElem.style.justifyContent = "center";
   rootElem.style.backgroundColor = "rgba(100,54,76,0.9)";
+  rootElem.appendChild(mainHeader);
+  mainHeader.style.display = "flex";
+  mainHeader.style.justifyContent = "center";
+  mainHeader.style.width = "100%";
+  mainHeader.style.height = "200px";
+
+  let mainHeaderLink = document.createElement("A");
+  mainHeaderLink.textContent =
+    "Game of Thrones Episodes, Extracted from TVMaze.com";
+  mainHeaderLink.href = "https://www.tvmaze.com/";
+  mainHeaderLink.target = "_blank";
+  mainHeaderLink.style.textDecoration = "none";
+  mainHeaderLink.style.color = "white";
+  mainHeader.style.margin = "170px 0 0 100px";
+  mainHeader.appendChild(mainHeaderLink);
+
   // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
   episodeList.forEach((episode) => {
     let currEpisode = document.createElement("div");
     let currEpisodeHeading = document.createElement("h2");
-    currEpisodeHeading.textContent = `${episode.name} - S0${
-      episode.season
-    }E${episode.number < 10 ? "0" + episode.number : episode.number}`;
+    let episodeLink = document.createElement("A");
+    episodeLink.href = episode._links.self.href.replace("api.", "");
+    episodeLink.target = "_blank";
+
+    episodeLink.textContent = `${episode.name} - S0${episode.season}E${
+      episode.number < 10 ? "0" + episode.number : episode.number
+    }`;
+    currEpisodeHeading.appendChild(episodeLink);
     let episodeDescription = document.createElement("p");
     episodeDescription.innerHTML = episode.summary;
     episodeDescription.addEventListener(
@@ -24,12 +47,11 @@ function makePageForEpisodes(episodeList) {
       function (event) {
         // highlight the mouseover target
         event.target.style.color = "orange";
-        
-          // reset the color after a short delay
-          setTimeout(function () {
-            event.target.style.color = "";
-            
-          }, 100);
+
+        // reset the color after a short delay
+        setTimeout(function () {
+          event.target.style.color = "";
+        }, 100);
       },
       false
     );
@@ -53,6 +75,11 @@ function makePageForEpisodes(episodeList) {
 
     currEpisodeHeading.style.border = "solid";
     currEpisodeHeading.style.borderRadius = "5%";
+
+    episodeLink.style.textDecoration = "none";
+    episodeLink.style.padding = "0 0 0 5%";
+    episodeLink.style.fontSize = "15px";
+    episodeLink.style.color = "white";
 
     episodeDescription.style.width = "90%";
     episodeDescription.style.fontSize = "13px";
