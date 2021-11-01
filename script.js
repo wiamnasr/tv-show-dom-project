440ec724b44b77e9d58e75ea54c6816dbca47a59
Wiam <wiamnasr@gmail.com>
2021-11-01 14:33:31 +0000

    design changes

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //You can edit ALL of the code here
 function setup() {
+
+  
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
@@ -79,12 +81,13 @@ function makePageForEpisodes(episodeList) {
   rootElem.style.display = "flex";
   rootElem.style.flexFlow = "row wrap";
   rootElem.style.justifyContent = "center";
-  rootElem.style.backgroundColor = "rgba(100,54,76,0.9)";
+  // rootElem.style.backgroundColor = "rgba(100,54,76,0.9)";
   rootElem.style.listStyleType = "none";
   rootElem.style.maxWidth = "100%";
 
   // // Styling the mainHeader
   mainHeader.style.display = "flex";
+  
   mainHeader.style.justifyContent = "center";
   mainHeader.style.width = "100%";
   mainHeader.style.height = "200px";
@@ -93,12 +96,15 @@ function makePageForEpisodes(episodeList) {
   mainHeader.style.backgroundRepeat = "no-repeat";
   mainHeader.style.backgroundSize = "cover";
   mainHeader.style.backgroundPosition = "center";
+  mainHeader.style.opacity = "0.5";
 
   // Styling the mainHeaderLink:
   mainHeaderLink.style.textShadow = "2px 2px 8px #FF0000";
   mainHeaderLink.style.textDecoration = "none";
   mainHeaderLink.style.color = "white";
   mainHeaderLink.style.margin = "80px 0 0 0";
+  
+  
 
   //selecting classes for episodes, episodesHeadings, episodesLinks, episodesImages and episodesDescriptions
   let episodes = document.getElementsByClassName("episodes");
@@ -123,7 +129,7 @@ function makePageForEpisodes(episodeList) {
         episodes[i].style.border = "solid";
         episodes[i].style.borderColor = "white";
         episodes[i].style.borderRadius = "2.5%";
-
+       
         // Styling the episodesHeadings:
         episodesHeadings[i].style.width = "90%";
         episodesHeadings[i].style.margin = "5px 0 0 5%";
@@ -149,20 +155,20 @@ function makePageForEpisodes(episodeList) {
         episodesDescriptions[i].style.overflowX = "scroll";
 
         /*
-        Starting here, those need to be in the styling bit and accounted for in the media query for page responsiveness
+        Starting here, those need to be changed into something prettier ()
     */
-        episodesDescriptions[i].removeEventListener(
-          "mouseover",
-          function (event) {
-            // highlight the mouseover target
-            event.target.style.color = "orange";
-            event.target.style.fontSize = "23px";
-            // currEpisode.style.height = "600px";
-            episodes[i].style.width = "23%";
-            episodes[i].style.height = "510px";
-          },
-          true
-        );
+        // episodesDescriptions[i].removeEventListener(
+        //   "mouseover",
+        //   function (event) {
+        //     // highlight the mouseover target
+        //     event.target.style.color = "orange";
+        //     event.target.style.fontSize = "23px";
+        //     // currEpisode.style.height = "600px";
+        //     episodes[i].style.width = "23%";
+        //     episodes[i].style.height = "510px";
+        //   },
+        //   true
+        // );
 
         /*
         Until here
@@ -208,25 +214,25 @@ function makePageForEpisodes(episodeList) {
         /*
         Starting here, those need to be in the styling bit and accounted for in the media query for page responsiveness
     */
-        episodesDescriptions[i].addEventListener(
-          "mouseover",
-          function (event) {
-            // highlight the mouseover target
-            event.target.style.color = "orange";
-            event.target.style.fontSize = "23px";
-            // currEpisode.style.height = "600px";
-            episodes[i].style.width = "23%";
-            episodes[i].style.height = "510px";
-          },
-          false
-        );
-        episodesDescriptions[i].addEventListener("mouseout", function (event) {
-          event.target.style.color = "";
-          event.target.style.fontSize = "13px";
-          // currEpisode.style.height = "500px";
-          episodes[i].style.width = "22%";
-          episodes[i].style.height = "500px";
-        });
+        // episodesDescriptions[i].addEventListener(
+        //   "mouseover",
+        //   function (event) {
+        //     // highlight the mouseover target
+        //     event.target.style.color = "orange";
+        //     event.target.style.fontSize = "23px";
+        //     // currEpisode.style.height = "600px";
+        //     episodes[i].style.width = "23%";
+        //     episodes[i].style.height = "510px";
+        //   },
+        //   false
+        // );
+        // episodesDescriptions[i].addEventListener("mouseout", function (event) {
+        //   event.target.style.color = "";
+        //   event.target.style.fontSize = "13px";
+        //   // currEpisode.style.height = "500px";
+        //   episodes[i].style.width = "22%";
+        //   episodes[i].style.height = "500px";
+        // });
         /*
         Until here
     */
