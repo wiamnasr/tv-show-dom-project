5e8059e27302a50d43342e88d4ad78731f454d60
Wiam <wiamnasr@gmail.com>
2021-11-09 05:48:13 +0000

    refactored and organized

--- script.js
+++ script.js
@@ -1,87 +1,107 @@
-// End
+//
+// //
+// // //
+// Global Variables:
+// // //
+// //
+//
 
-let showSelect = document.getElementById("shows");
+const rootElem = document.getElementById("root");
+const showsDropdown = document.getElementById("shows");
+const episodesDropdown = document.getElementById("episodes");
 
-// You can edit ALL of the code here
+const mainHeader = document.createElement("h1");
+rootElem.appendChild(mainHeader);
+
+const mainHeaderLink = document.createElement("a");
+mainHeaderLink.textContent = `Episodes Extracted from TVMaze.com`;
+mainHeaderLink.href = "https://www.tvmaze.com/";
+mainHeaderLink.target = "_blank";
+mainHeader.appendChild(mainHeaderLink);
+
+//
+// //
+// // //
+// Async Functions:
+// // //
+// //
+//
+
+const selectedEp = async function () {
+  let userShowChoiceValue = document.getElementById("shows").value;
+  const response = await fetch(
+    `https://api.tvmaze.com/shows/${userShowChoiceValue}/episodes`
+  );
+
+  const jsonObject = await response.json();
+
+  makePageForEpisodes(jsonObject);
+};
+
+//
+// //
+// // //
+// Functions:
+// // //
+// //
+//
+
 function setup() {
   const showsArr = getAllShows();
 
   showsArr.forEach((showObject) => {
-    let newOption = document.createElement("option");
-    newOption.value = showObject.id;
-    newOption.innerText = showObject.name;
-    showSelect.appendChild(newOption);
+    let newShowOption = document.createElement("option");
+    newShowOption.value = showObject.id;
+    newShowOption.innerText = showObject.name;
+    showsDropdown.appendChild(newShowOption);
   });
-
-  // .catch((error) => console.error("error occured: " + error));
 }
 
-//selecting the "root" div in the html
-const rootElem = document.getElementById("root");
+function createEpisodeCard(episode) {
+  let currEpisode = document.createElement("li");
+  currEpisode.classList.add("episodes");
+  let currEpisodeHeading = document.createElement("h2");
+  currEpisodeHeading.classList.add("episodesHeadings");
+  let episodeLink = document.createElement("A");
+  episodeLink.classList.add("episodesLinks");
+  episodeLink.href = episode._links.self.href.replace("api.", "");
+  episodeLink.target = "_blank";
 
-//creating a header that will contain the title of the page that is linked to tv maze website
-let mainHeader = document.createElement("h1");
-rootElem.appendChild(mainHeader);
+  episodeLink.textContent = `${episode.name} - S0${episode.season}E${
+    episode.number < 10 ? "0" + episode.number : episode.number
+  }`;
 
-let mainHeaderLink = document.createElement("a");
-mainHeaderLink.textContent = "GOT Episodes, Extracted from TVMaze.com";
-mainHeaderLink.href = "https://www.tvmaze.com/";
-mainHeaderLink.target = "_blank";
-mainHeader.appendChild(mainHeaderLink);
-//edit this to display the total number of episodes:
-// rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  currEpisodeHeading.appendChild(episodeLink);
+
+  let episodeImg = document.createElement("img");
+  episodeImg.classList.add("episodesImages");
+  episodeImg.src = episode.image.medium;
+
+  let episodeDescription = document.createElement("p");
+  episodeDescription.classList.add("episodesDescriptions");
+
+  episodeDescription.innerHTML = episode.summary;
+
+  currEpisode.appendChild(currEpisodeHeading);
+  currEpisode.appendChild(episodeImg);
+  currEpisode.appendChild(episodeDescription);
+
+  return currEpisode;
+}
 
 // this function renders the episodes on the page
 function makePageForEpisodes(episodeList) {
   rootElem.innerHTML = "";
-  let subjectSel = document.getElementById("subject");
+  rootElem.appendChild(mainHeader);
+  episodesDropdown.innerHTML = "";
   episodeList.forEach((episode) => {
-    let currEpisode = document.createElement("li");
-    currEpisode.classList.add("episodes");
-    let currEpisodeHeading = document.createElement("h2");
-    currEpisodeHeading.classList.add("episodesHeadings");
-    let episodeLink = document.createElement("A");
-    episodeLink.classList.add("episodesLinks");
-    episodeLink.href = episode._links.self.href.replace("api.", "");
-    episodeLink.target = "_blank";
-
-    episodeLink.textContent = `${episode.name} - S0${episode.season}E${
-      episode.number < 10 ? "0" + episode.number : episode.number
-    }`;
-
-    subjectSel.options[subjectSel.options.length] = new Option(
+    rootElem.appendChild(createEpisodeCard(episode));
+    episodesDropdown.options[episodesDropdown.options.length] = new Option(
       `S0${episode.season}E${
         episode.number < 10 ? "0" + episode.number : episode.number
       } - ${episode.name}`
     );
-    currEpisodeHeading.appendChild(episodeLink);
-
-    let episodeImg = document.createElement("img");
-    episodeImg.classList.add("episodesImages");
-    episodeImg.src = episode.image.medium;
-
-    let episodeDescription = document.createElement("p");
-    episodeDescription.classList.add("episodesDescriptions");
-
-    episodeDescription.innerHTML = episode.summary;
-
-    rootElem.appendChild(currEpisode);
-    currEpisode.appendChild(currEpisodeHeading);
-    currEpisode.appendChild(episodeImg);
-    currEpisode.appendChild(episodeDescription);
   });
-  subjectSel.onchange = function () {
-    let userSelected = subjectSel.value.substring(0, 6).toUpperCase();
-    let documentEpisodes = rootElem.getElementsByTagName("li");
-    let episodeNames = document.getElementsByClassName("episodesLinks");
-    for (let i = 0; i < episodeNames.length; i++) {
-      if (episodeNames[i].textContent.toUpperCase().includes(userSelected)) {
-        documentEpisodes[i].style.display = "";
-      } else {
-        documentEpisodes[i].style.display = "none";
-      }
-    }
-  };
 }
 
 function userSearchFunction() {
@@ -123,16 +143,34 @@ function userSearchFunction() {
   }
 }
 
-const selectedEp = async function () {
-  let userShowChoiceValue = document.getElementById("shows").value;
-  const response = await fetch(
-    `https://api.tvmaze.com/shows/${userShowChoiceValue}/episodes`
-  );
+function displayOnlySelectedEpisode() {
+  let userSelectedEpisode = episodesDropdown.value
+    .substring(0, 6)
+    .toUpperCase();
+  console.log(userSelectedEpisode);
 
-  const jsonObject = await response.json();
-  makePageForEpisodes(jsonObject);
-};
+  let displayedEpisodes = rootElem.getElementsByTagName("li");
+  let episodeNames = document.getElementsByClassName("episodesLinks");
+  for (let i = 0; i < episodeNames.length; i++) {
+    if (
+      episodeNames[i].textContent.toUpperCase().includes(userSelectedEpisode)
+    ) {
+      displayedEpisodes[i].style.display = "";
+    } else {
+      displayedEpisodes[i].style.display = "none";
+    }
+  }
+}
+
+//
+// //
+// // //
+// Listeners and Friends:
+// // //
+// //
+//
+
+episodesDropdown.onchange = displayOnlySelectedEpisode;
+showsDropdown.onchange = selectedEp;
 
 window.onload = setup;
-// remember to change var names to something less confusing
-showSelect.onchange = selectedEp;
